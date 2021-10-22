5455aa49b8273b7840db7dd8353ef1ae34f60fe4
vischio <rns11aiwbk@libero.it>
2021-10-22 14:46:24 +0200

    change center with flex add a function to draw the grid add prompt

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const container = document.querySelector("#container");
 
-for(let i = 0; i < 16; i++) { 
-    const flexRow = document.createElement('div');
-    flexRow.classList.add("row");
-    container.appendChild(flexRow);
+drawGrid();
 
-    for(let j = 0; j < 16; j++) { 
-        const flexColumn = document.createElement('div');
-        flexRow.appendChild(flexColumn);
-        flexColumn.textContent = j;
-        flexColumn.classList.add("box", "baseColor");
-        flexColumn.addEventListener("mouseover", changeColor );
-        flexColumn.addEventListener("mouseleave", baseColor);
+function drawGrid(side = 16) {
+
+    for(let i = 1; i <= side; i++) { 
+        const flexRow = document.createElement('div');
+        flexRow.classList.add("row");
+        container.appendChild(flexRow);
+
+        for(let j = 1; j <= side; j++) { 
+            const flexColumn = document.createElement('div');
+            flexRow.appendChild(flexColumn);
+            flexColumn.textContent = j;
+            flexColumn.classList.add("box", "baseColor");
+            flexColumn.addEventListener("mouseover", changeColor );
+            flexColumn.addEventListener("mouseleave", baseColor);
+        }
     }
 }
 
@@ -25,3 +30,17 @@ function baseColor (event) {
     event.target.classList.add("baseColor");
 }
 
+/** 
+ *  BUTTON EVENT
+ */
+const button = document.querySelector('button');
+button.addEventListener('click', numberRequest);
+
+function numberRequest (event) {
+    const side = +prompt("enter a number between 1 and 100 inclusive ( default = 16)");
+    container.innerHTML = "";
+    if(isNaN(side) || side < 1 || side > 100) 
+        drawGrid();
+    else
+        drawGrid(side);
+}
